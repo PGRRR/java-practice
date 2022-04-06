ba59e941b62c3092ae1b8b58c9da779d0df63348
PGRRR <PGRRR119@gmail.com>
2022-04-06 16:59:46 +0900

    2022-04-06 6-1교시

--- src/ch1/OperationPromotionExample.java
+++ src/ch1/OperationPromotionExample.java
@@ -5,23 +5,23 @@ public class OperationPromotionExample {
 	public static void main(String[] args) {
 		byte byteValue1 = 10;
 		byte byteValue2 = 20;
-		//byte byteValue3 = byteValue1 + byteValue2;	//ì»´íŒŒì¼ ì—ëŸ¬
+		//byte byteValue3 = byteValue1 + byteValue2;	//ÄÄÆÄÀÏ ¿¡·¯
 		int intValue1 = byteValue1 + byteValue2;
 		System.out.println(intValue1);
 		
 		char charValue1 = 'A';
 		char charValue2 = 1;
-		//char charValue3 = charValue1 + charValue2;	//ì»´íŒŒì¼ ì—ëŸ¬
+		//char charValue3 = charValue1 + charValue2;	//ÄÄÆÄÀÏ ¿¡·¯
 		int intValue2 = charValue1 + charValue2;
-		System.out.println("ìœ ë‹ˆì½”ë“œ = " + intValue2);
-		System.out.println("ì¶œë ¥ë¬¸ì = " + (char)intValue2);
+		System.out.println("À¯´ÏÄÚµå = " + intValue2);
+		System.out.println("Ãâ·Â¹®ÀÚ = " + (char)intValue2);
 		
 		int intValue3 = 10;
 		int intValue4 = intValue3/4;
 		System.out.println(intValue4);
 		
 		int intValue5 = 10;
-		//int intValue6 = 10 / 4.0;	//ì»´íŒŒì¼ ì—ëŸ¬
+		//int intValue6 = 10 / 4.0;	//ÄÄÆÄÀÏ ¿¡·¯
 		double doubleValue = intValue5 / 4.0;
 		System.out.println(doubleValue);
 
