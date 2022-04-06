daedb64a82aa5d52f3c8b3486251567e839b1b29
PGRRR <PGRRR119@gmail.com>
2022-04-06 11:27:38 +0900

    1 교시

---
+++ src/ch1/VariableExample.java
@@ -0,0 +1,18 @@
+package ch1;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		//10�� 蹂��� value�� 珥�湲곌��쇰� ����
+		int value = 10;
+		
+		//蹂��� value 媛��� �쎄� 10�� ������ �곗�� �곗�곗�� ����!
+		//�곗�곗�� 寃곌낵媛��� 蹂��� result�� 珥�湲곌��쇰� ����
+		int result = value + 10;
+		
+		//蹂��� result 媛��� �쎄� 肄����� 異���
+		System.out.println(result);
+
+	}
+
+}
