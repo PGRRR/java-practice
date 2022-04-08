d87672c031acbd75b11a2d7f4f0affc791606d8e
PGRRR <PGRRR119@gmail.com>
2022-04-08 16:42:29 +0900

    JAVA - switch, operator

---
+++ src/ch1/ConditionalOperationExample0408.java
@@ -0,0 +1,12 @@
+package ch1;
+
+public class ConditionalOperationExample0408 {
+
+	public static void main(String[] args) {
+		int score = 95;
+		char grade = (score > 90) ? 'A' : ((score > 80 ? 'B' : 'C'));
+		System.out.println(score + "점은 " + grade + "등급입니다.");
+
+	}
+
+}
---
+++ src/ch1/Test2_0408.java
@@ -0,0 +1,30 @@
+package ch1;
+
+import java.util.Scanner;
+
+public class Test2_0408 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("계절 월로 입력");
+		int month = sc.nextInt();
+		switch (month) {
+		case 3: case 4: case 5:
+			System.out.println("봄");
+			break;
+		case 6: case 7: case 8:
+			System.out.println("여름");
+			break;
+		case 9: case 10: case 11:
+			System.out.println("가을");
+			break;
+		case 12: case 1: case 2:
+			System.out.println("겨울");
+			break;
+		default:
+			System.out.println("잘못된 입력");
+		}
+
+	}
+
+}
