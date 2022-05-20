568965400167621148e326a49d9a4d0fea9496b0
pgrrr <pgrrr119@gmail.com>
2022-05-20 16:59:21 +0900

    java - spring tdd, spring service

---
+++ src/ch16/Key.java
@@ -0,0 +1,19 @@
+package ch16;
+
+public class Key {
+    public int number;
+
+    public Key(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Key) {
+            Key compareKey = (Key) obj;
+            if (this.number == compareKey.number) {
+                return true;
+            }
+        }
+    }
+}
---
+++ src/test/java/nosleepcoders/holeinone/HoleInOneApplicationTests.java
@@ -0,0 +1,13 @@
+package nosleepcoders.holeinone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoleInOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
